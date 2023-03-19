9715cd10e5a5138ad35887425691d0c7a1edbb5c
Asmakhfaoui <asmaamakhfaoui@gmail.com>
2023-03-19 15:48:34 -0700

    commiommit this changes one character

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,6 @@ int main(void)
 	printf("Size of an int:%2lu byte(s)\n", sizeof(int));
 	printf("Size of a long int:%2lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int:%2lu byte(s)\n", sizeof(long long int));
-	printf("Size of float:%2lu byte(s)\n", sizeof(float));
+	printf("Size of a float:%2lu byte(s)\n", sizeof(float));
 	return (0);
 }
