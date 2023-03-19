17fdcaac105b15b4cac2496483af38e9da51720b
Asmakhfaoui <asmaamakhfaoui@gmail.com>
2023-03-19 15:44:06 -0700

    commiommit this changes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
- *main entry function
- *return : should be 0 for success
+ *main - entry point
+ *Description: 'sizeof characters'
+ *Return: should be 0 for success
  */
 int main(void)
 {
-	printf("Size of a char:%lu byte(s)", sizeof(char));
-	printf("Size of an int:%lu byte(s)", sizeof(int));
-	printf("Size of a long int:%lu byte(s)", sizeof(long int));
-	printf("Size of a long long int:%lu byte(s)", sizeof(long long int));
-	printf("Size of float:%lu  byte(s)", sizeof(float));
+	printf("Size of a char:%2lu byte(s)\n", sizeof(char));
+	printf("Size of an int:%2lu byte(s)\n", sizeof(int));
+	printf("Size of a long int:%2lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int:%2lu byte(s)\n", sizeof(long long int));
+	printf("Size of float:%2lu byte(s)\n", sizeof(float));
 	return (0);
 }
