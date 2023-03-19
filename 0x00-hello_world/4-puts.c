0c88474a5573f061a32f02fd3d90c9d379f75af2
Asmakhfaoui <asmaamakhfaoui@gmail.com>
2023-03-19 15:34:05 -0700

    solving errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * it's a short description of main function
- * it return value is equal to 0
+ *main - entry point
+ *Description: 'main function'
+ *Return: value is equal to 0 for success
  */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
